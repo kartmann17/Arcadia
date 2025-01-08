54aa4cd39ebb3060e2c75db4136a7454fd1f2ffe
kartmann17 <dronex.contact@gmail.com>
2025-01-08 14:49:58 +0100

    index public

--- public/index.php
+++ public/index.php
@@ -2,23 +2,20 @@
 
 use App\Autoloader;
 use App\config\Main;
+use Dotenv\Dotenv;
 
 // Inclure l'autoloader de Composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Vérifier et charger le fichier .env uniquement s'il existe (pour local)
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
-}
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-// Définir une constante avec le chemin racine du projet
+// Définition d'une constante avec le chemin racine du projet
 define('ROOT', dirname(__DIR__));
 
-// Enregistrer l'autoloader
 require_once ROOT . '/Autoloader.php';
 Autoloader::register();
 
-// Démarrer l'application
 $app = new Main();
+
 $app->start();
